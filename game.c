3baacbe2c4c5290552711fabbc9c401dfe12b52e
parthsharma07ongod <madhavisharma1975@gmail.com>
2025-12-03 17:24:43 +0530

    Updated game.c with new features ^_____^

--- game.c
+++ game.c
@@ -3,8 +3,6 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <time.h>
-/*for bg sound*/
-#include <mmsystem.h>
 
 /*to remove flicker*/
 void clear_screen_fast() {
@@ -14,19 +12,29 @@ void clear_screen_fast() {
 }
 
 int main() {
-	system("color 4F");
-	PlaySound(TEXT("bg.wav"), NULL, SND_ASYNC | SND_LOOP);
     srand(time(0));
 
     int x = 1;              // player position (0 to 2)
-    int step = 0;           // obstacle vertical movement
-    int obstaclePos = rand() % 3;   // 0,1,2 lane
+    int step1 = 0;           // obstacle 1 vertical movement
+    int obstaclePos1 = rand() % 3;   // 0,1,2 lane
+    int step2 = -5;              // obstacle 2 vertical movement
+    int obstaclePos2 = rand()%3;
+    int score=0;
+    int lives=3;
+    int gameOver=0;
+    while(obstaclePos2==obstaclePos1){     //obstacles not in same lane
+            obstaclePos2=rand()%3;
+        }
 
     while (1) {
 
         // ---- INPUT ----
         if (_kbhit()) {
-            char ch = getch();
+            int ch = getch();
+
+            if(ch==224){
+                ch=getch();
+            }
 
             if (ch == 75 && x > 0)        // LEFT arrow
                 x--;
@@ -41,49 +49,76 @@ int main() {
         printf("|--- --- ---|\n");
 
         for (int i = 0; i < 10; i++) {
-            if (i == step) {
+            if (i == step1 || i == step2) {
 
-                if (obstaclePos == 0)
-                    printf("| %c        |\n", 1);
+                if (i == step1 && obstaclePos1 == 0) printf("| *        |\n");
+                else if (i == step1 && obstaclePos1 == 1) printf("|     *    |\n");
+                else if (i == step1 && obstaclePos1 == 2) printf("|        * |\n");
 
-                else if (obstaclePos == 1)
-                    printf("|     %c    |\n", 1);
+                else if(i == step2 && obstaclePos2 == 0) printf("| *        |\n");
+                else if(i == step2 && obstaclePos2 == 1) printf("|     *    |\n");
+                else if(i == step2 && obstaclePos2 == 2) printf("|        * |\n");
 
-                else if (obstaclePos == 2)
-                    printf("|        %c |\n", 1);
+                else printf("|           |\n");
 
-            } else {
+            } 
+            else {
                 printf("|           |\n");
             }
         }
 
         // ---- PLAYER ----
         if (x == 0)
-            printf("| %c         |\n", 6);
+            printf("| ^         |\n");
         else if (x == 1)
-            printf("|     %c     |\n", 6);
+            printf("|     ^     |\n");
         else if (x == 2)
-            printf("|        %c  |\n", 6);
+            printf("|        ^  |\n");
+            if(lives>0)
 
         // ---- COLLISION ----
-        if (step == 10 && x == obstaclePos) {
-        	PlaySound(NULL, NULL, 0);  // stop background
-			PlaySound(TEXT("impact.wav"), NULL, SND_ASYNC);
-        	Sleep(2500);
-            printf("\nGAME OVER!\n");
-            break;
+        if ((step1 == 10 && x == obstaclePos1) || (step2 == 10 && x == obstaclePos2)) {
+        	Sleep(500);
+            lives--;
+            if(lives<=0){
+                lives=0;
+                gameOver=1;
+            }
+            if(!gameOver){
+            step1=0;
+            obstaclePos1=rand()%3;
+
+            step2=-5;
+            obstaclePos2=rand()%3;
+            }
         }
+                printf("\n\n\n\nScore: %d | Lives: %d\n",score,lives);
+            if(gameOver){
+                printf("\nGame Over! Such a noob >.<\n");
+                printf("Your Final Score: %d\n",score);
+                break;
+            }
 
         Sleep(120);
 
-        // Move obstacle down
-        step++;
-
-        // Reset when reaches bottom
-        if (step > 10) {
-            step = 0;
-            obstaclePos = rand() % 3; // new lane
+        // Obstacle 1
+        step1++;
+        if (step1 > 10) {
+            step1 = 0;
+            obstaclePos1 = rand() % 3; // new lane
+            score++;
         }
+        // Obstacle 2
+        step2++;
+        if(step2 > 10){
+            step2 = -5;
+            obstaclePos2 = rand()%3;
+            score++;
+            while(obstaclePos2==obstaclePos1){
+            obstaclePos2=rand()%3;
+            }
+        }
+        
     }
 
     return 0;
